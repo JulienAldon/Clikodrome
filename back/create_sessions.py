d88c3dbed66f57e4ce93c39b1cc99b17ca052617
Julien <julien.aldon@epitech.eu>
2022-11-13 19:49:55 +0100

    Add cli commands for session management

---
+++ back/create_sessions.py
@@ -0,0 +1,15 @@
+from src.sessions import create_single_session
+import datetime
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("session_index", choices=['0', '-1'], help="set the session index, 0 for earliest, -1 for latest session")
+    parser.add_argument("--date", help="set the date (format YYYY-MM-DD")
+    args = parser.parse_args()
+    if args.date:
+        format_date = args.date
+    else:
+        session_date = datetime.datetime.today()
+        format_date = session_date.strftime('%Y-%m-%d')
+    create_single_session(format_date, int(args.session_index))
---
+++ back/sign_sessions.py
@@ -0,0 +1,15 @@
+from src.sessions import sign_all_sessions
+import datetime
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("session_index", choices=['0', '-1'], help="set the session index, 0 for earliest session, -1 for latest session")
+    parser.add_argument("--date", help="set the date (format YYYY-MM-DD)")
+    args = parser.parse_args()
+    if args.date:
+        current_date = args.date
+    else:
+        current_date = datetime.datetime.today().strftime('%Y-%m-%d')[:10]
+    session_index = args.session_index
+    sign_all_sessions(current_date, int(session_index))
