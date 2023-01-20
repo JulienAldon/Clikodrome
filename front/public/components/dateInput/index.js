76c30d8763a883d87f4be9a4b5a94857527b91ef
Julien <julien.aldon@epitech.eu>
2023-01-20 11:07:04 +0100

    Clean dateInput comments

--- front/public/components/dateInput/index.js
+++ front/public/components/dateInput/index.js
@@ -3,7 +3,6 @@ import styles from './style.module.css';
 export default function DateInput(props) {
     return (
         <div class={styles.center}>
-            {/* <label class={styles.label}for="date" >{props.title}</label> */}
             <input 
                 id="date"
                 class={styles.search}
